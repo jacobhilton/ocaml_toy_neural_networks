c6e4a866f74680237ab8a1e69a997467c0c921b9
Jacob hilton <jacob.hilton@gmail.com>
2017-04-30 13:02:46 +0000

    initial commit

---
+++ main.ml
@@ -0,0 +1,16 @@
+open Core
+open Async
+
+let () =
+  let open Command.Let_syntax in
+  Command.async'
+    ~summary:"cook eggs"
+    [%map_open
+      let num_eggs =
+        flag "num-eggs" (required int) ~doc:"COUNT cook this many eggs"
+      in
+      fun () ->
+        (* TODO: implement egg-cooking in ocaml *)
+        failwith "no eggs today"
+    ]
+  |> Command.run
