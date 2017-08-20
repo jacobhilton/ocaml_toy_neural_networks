b3fbceda3fc7c96433076b01667aa4dfa029d95b
Jacob hilton <jacob.hilton@gmail.com>
2017-08-20 13:54:53 +0000

    Infinite_list.split_n

--- infinite_list.ml
+++ infinite_list.ml
@@ -23,6 +23,15 @@ let nth_exn { list; default } n =
   | Neg -> failwithf "Infinite_list.nth_exn %i called" n ()
   | Zero | Pos -> Option.value (List.nth list n) ~default
 
+let split_n { list; default } n =
+  let diff = Int.(-) (List.length list) n in
+  match Int.sign diff with
+  | Zero -> list, constant ~default
+  | Pos ->
+    let list1, list2 = List.split_n list n in
+    list1, { list = list2; default }
+  | Neg -> list @ (List.init (Int.abs diff) ~f:(Fn.const default)), constant ~default
+
 let map { list; default } ~f =
   { list = List.map list ~f
   ; default = f default
--- infinite_list.mli
+++ infinite_list.mli
@@ -8,6 +8,8 @@ val e_i : int -> zero:'a -> one:'a -> 'a t
 
 val nth_exn : 'a t -> int -> 'a
 
+val split_n : 'a t -> int -> 'a list * 'a t
+
 val map : 'a t -> f:('a -> 'b) -> 'b t
 
 val map2 : 'a t -> 'b t -> f:('a -> 'b -> 'c) -> 'c t
